99a6af3283433f890fbcd328453759aac54578fe
Crownist <202111242@fit.edu.ph>
2024-07-17 00:16:14 +0800

    hehehe

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import {useState, useEffect} from "react";
 import {ethers} from "ethers";
 import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
-
+//...
 export default function HomePage() {
   const [ethWallet, setEthWallet] = useState(undefined);
   const [account, setAccount] = useState(undefined);
